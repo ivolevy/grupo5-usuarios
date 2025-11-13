945d3a113e80a55e915acd4cd60dd6ed4dcd1aff
TIZIANO <grecotizianoweb@gmail.com>
2025-11-12 21:01:41 -0300

    Update page.tsx

--- app/swagger/page.tsx
+++ app/swagger/page.tsx
@@ -2,6 +2,9 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
+// Force dynamic rendering to avoid build errors during static generation
+export const dynamic = 'force-dynamic';
+
 export default function SwaggerPage() {
   return (
     <div style={{ height: '100%', minHeight: '100vh' }}>
